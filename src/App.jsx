d4405670290aefc20cf54c0e8370be1e57f44e7e
soujanya hazra <soujanya0105@gmail.com>
2023-08-03 03:42:11 +0530

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='main'>
-      <App/>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Header.module.css"
+import heroSectionSVG from "../../assets/heroSectionSVG.svg"
+import Button from 'react-bootstrap/Button';
+
+function Header() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h1 className={styles.heading}>
+                    Unleash Your Potential with our <span>Resume</span> Builder!
+                </h1>
+                <p className={styles.para}>
+                    Craft Your Career Journey for free !
+                </p>
+                <div className={styles.heroSectionCTA}>
+                    <Button variant="primary">Get Started</Button>
+                </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <img src={heroSectionSVG} alt="Resume" />
+            </div>
+        </div>
+    );
+
+}
+
+export default Header 
