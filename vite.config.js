d6c63c55850bd9a2ef8d3014e1864307d7b73da3
soujanya hazra <soujanya0105@gmail.com>
2023-08-03 03:40:54 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), reactRefresh()],
+  css: {
+    modules: {
+      localsConvention: "camelCase"
+    }
+  }
+})
