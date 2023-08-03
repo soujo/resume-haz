7b378a00df88cacea72a1d7a470c2cbbcceada48
soujanya hazra <soujanya0105@gmail.com>
2023-08-04 02:46:46 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/Header/Header'
 import Body from './components/Body/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className='main'>
